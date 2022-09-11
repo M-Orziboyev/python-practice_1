6f20a6c50751912bad9494df8a7e30a04d6ca4fa
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2022-09-11 16:23:58 +0500

    Python practical_1

---
+++ amaliy1.py
@@ -0,0 +1,34 @@
+# Amaliyot 1
+
+
+#SAVOL
+#Quyidagi matnni aynan shunday ko'rinishda konsolda chiqaring
+# "Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+
+#JAVOB
+print(" \"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas.")
+
+
+#SAVOL
+#Quyidagi misollarga yechimni pythonda chiqaring. Har bir misoldan avval mattin izoh ko'rinishida yozing. Javoblarni savol va javob ko'rinishida chiqaring!
+# 5 ning 4-darajasini toping.
+
+#JAVOB
+print(5**4) #625
+
+#SAVOL 
+# 22ni 4ga bo'lganda qancha qoldiq qoladi? 
+
+#JAVOB 
+
+print(22%4)  # 2 qoldiq
+ 
+ #SAVOL
+ # Tomonlari 125ga teng bo'lgan doiraning yuzini toping.
+ 
+ #JAVOB 
+ 
+print(2 * 125 *3.14) #785
+
+
+
